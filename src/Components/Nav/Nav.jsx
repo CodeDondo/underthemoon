18f4331396d1d01dfee827a163ab93a06bd08887
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2024-12-12 13:15:58 +0100

    nav-changes

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,15 @@
 import styles from './Nav.module.scss';
+import { Link } from 'react-router-dom';
 
 export const Nav = () => {
     return (
         <nav className={styles.Nav}>
             <ul className={styles.navItems}>
-                <li><a href="#">Hjem</a></li>
-                <li><a href="#">Koncepter</a></li>
-                <li><a href="#">UTM Loyalty</a></li>
-                <li><a href="#">Om</a></li>
-                <li><a href="#">Kontakt</a></li>
+                <li><Link to='/'>Hjem</Link></li>
+                <li><Link to='Koncepter'>Koncepter</Link></li>
+                <li><Link to='UTMLoyalty'>UTM Loyalty</Link></li>
+                <li><Link to='Om'>Om</Link></li>
+                <li><Link to='Kontakt'>Kontakt</Link></li>
             </ul>
         </nav>
     );
